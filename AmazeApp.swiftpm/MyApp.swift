e4875a7f28ae62aae79e7195893e3fe1caa168de
Mr Jeff <mrjeff@musicalgizmos.com>
2022-01-19 11:49:37 -0500

    code cleanup

--- AmazeApp.swiftpm/MyApp.swift
+++ AmazeApp.swiftpm/MyApp.swift
@@ -7,28 +7,7 @@ struct MyApp: App {
     @Environment(\.scenePhase) var scenePhase
     
     @StateObject var store = DataStore()
-    
-    // Called on App Launch
-//    init() {
-//        loadJSON()
-//    }
-    
-//    func loadJSON() {
-//        if let documentDirectory = FileManager.default.urls(for: .documentDirectory,
-//                                                               in: .userDomainMask).first {
-//            let filenameWithPath = documentDirectory.appendingPathComponent(jsonStringFileName)
-//            do {
-//                let jsonString = try String(contentsOf: filenameWithPath, encoding: .utf8)
-//                if let dataFromJsonString = jsonString.data(using: .utf8) {
-//                    let classrooms = try JSONDecoder().decode([Classroom].self,
-//                                                                from: dataFromJsonString)
-//                    store.classrooms = classrooms
-//                }
-//            }
-//            catch {/* error handling here */}
-//        }
-//    }
-    
+
     func makeJSONstring() -> String? {
         do {
             let encodedData = try JSONEncoder().encode(store.classrooms)
@@ -58,7 +37,6 @@ struct MyApp: App {
                 .onChange(of: scenePhase) { newPhase in
                     if newPhase == .active {
                     } else if newPhase == .inactive {
-//                        store.storeJSON()
                         saveJSONtoDisc()
                     } else if newPhase == .background {
                     }
